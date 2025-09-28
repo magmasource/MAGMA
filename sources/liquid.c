109ff1e722cad79a9cfe1af72be3d399384c853b
Paula Antoshechkina <psmith@gps.caltech.edu>
2025-09-28 08:45:53 -0700

    Disable GSL error handler during equilibration.
    This emulates the old behavior where some singular matrices were ignored.
    Also disable abort() in liquid routines for easyMelts and Melts-batch.
    Can still catch negative mole fraction from the library version.

--- sources/liquid.c
+++ sources/liquid.c
@@ -6948,11 +6948,13 @@ order(int mask, double t, double p, double r[NR],
             for (i=0;  i<NR; i++) printf("   %20.20s %13.6g %13.6g\n",         liquid[i+1].label,  xSpecies[i+1],  r[i]);
             for (i=0;  i<NS; i++) printf("   %20.20s %13.6g %13.13s %13.6g\n", liquid[i+NA].label, xSpecies[i+NA], "", sNew[i]);
             for (i=NS; i<NT; i++) printf("   %20.20s %13.13s %13.13s %13.6g\n", "order CN[*]", "", "", sNew[i]);
+#ifdef TESTDYNAMICLIB
 #ifdef USESEH
             doInterrupt = TRUE;
             raise_sigabrt(EXCEPTION_FLT_INVALID_OPERATION);
 #else
             (void) raise(SIGABRT);
+#endif
 #endif
         }
 
